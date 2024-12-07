a24cc549b0e4f6ff8d222111f716b0975b26a243
Christos Gkantidis <cgkantid@proton.me>
2024-12-08 00:01:01 +0100

    Small restructuring

--- src/utility.hpp
+++ src/utility.hpp
@@ -1,14 +1,14 @@
 #ifndef UTILITY_HPP
 #define UTILITY_HPP
 
-#include <cstdint> // std::uint8_t
+#include <cstdint>
 #include <string_view>
 #include <vector>
 
 bool
 is_digit(char ch);
 
-std::uint8_t
+uint8_t
 char_to_int(char ch);
 
 std::size_t
@@ -16,5 +16,11 @@ str_to_int(std::string_view sv);
 
 std::vector<std::string_view>
 split(std::string_view sv, std::string_view delim = " ");
+
+std::uint8_t
+get_num_digits(std::uint64_t num);
+
+std::uint64_t
+pow10(std::uint8_t exp);
 
 #endif // UTILITY_HPP
