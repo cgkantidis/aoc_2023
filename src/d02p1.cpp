1c0a3eea16a920561cfe2c1e15820303e43da345
Christos Gkantidis <cgkantid@proton.me>
2025-09-06 13:30:11 +0200

    Refresh d02 dolutions and add tests

--- src/d02p1.cpp
+++ src/d02p1.cpp
@@ -1,10 +1,9 @@
-#include <algorithm>
-#include <fmt/core.h>
-#include <fstream>
-#include <string>
-#include <vector>
-
 #include "utility.hpp"
+
+#include <algorithm> // std::ranges::all_of
+#include <print> // std::println
+#include <string> // std::string
+#include <vector> //std::vector
 
 struct GameSet
 {
@@ -19,6 +18,71 @@ struct Game
   std::vector<GameSet> m_sets;
 };
 
+namespace
+{
+void
+tests();
+std::uint64_t
+get_sum_of_ids_of_possible_games(std::ranges::range auto &&lines);
+Game
+parse_game(std::string_view line);
+std::size_t
+parse_game_id(std::string_view game_id_line);
+std::vector<GameSet>
+parse_game_sets(std::string_view game_sets_line);
+GameSet
+parse_game_set(std::string_view game_set_line);
+} // namespace
+
+int
+main(int argc, char const **argv) {
+  tests();
+  std::vector<std::string> lines = read_program_input(argc, argv);
+  std::println("{}", get_sum_of_ids_of_possible_games(lines));
+  return 0;
+}
+
+namespace
+{
+void
+tests() {
+  using namespace std::literals::string_view_literals;
+  auto const lines = std::array{
+      "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"sv,
+      "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue"sv,
+      "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red"sv,
+      "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red"sv,
+      "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"sv,
+  };
+  ASSERT(get_sum_of_ids_of_possible_games(lines) == 8);
+}
+
+std::uint64_t
+get_sum_of_ids_of_possible_games(std::ranges::range auto &&lines) {
+  static constexpr std::size_t MAX_RED = 12;
+  static constexpr std::size_t MAX_GREEN = 13;
+  static constexpr std::size_t MAX_BLUE = 14;
+
+  std::uint64_t total{};
+  for (auto const &line : lines) {
+    auto game = parse_game(line);
+    if (std::ranges::all_of(game.m_sets, [](GameSet const &game_set) {
+          return game_set.m_red <= MAX_RED && game_set.m_green <= MAX_GREEN
+                 && game_set.m_blue <= MAX_BLUE;
+        })) {
+      total += game.m_id;
+    }
+  }
+  return total;
+}
+
+Game
+parse_game(std::string_view line) {
+  auto tokens = split(line, ": ");
+  return Game{.m_id = parse_game_id(tokens[0]),
+              .m_sets = parse_game_sets(tokens[1])};
+}
+
 std::size_t
 parse_game_id(std::string_view game_id_line) {
   static constexpr std::size_t GAME_LEN{5 /*Game */};
@@ -29,9 +93,17 @@ parse_game_id(std::string_view game_id_line) {
   return game_id;
 }
 
+std::vector<GameSet>
+parse_game_sets(std::string_view game_sets_line) {
+  std::vector<GameSet> game_sets;
+  auto tokens = split(game_sets_line, "; ");
+  std::ranges::transform(tokens, std::back_inserter(game_sets), parse_game_set);
+  return game_sets;
+}
+
 GameSet
 parse_game_set(std::string_view game_set_line) {
-  GameSet game_set{0, 0, 0};
+  GameSet game_set{.m_red = 0, .m_green = 0, .m_blue = 0};
   for (std::string_view pair : split(game_set_line, ", ")) {
     auto tokens = split(pair);
     std::size_t num{};
@@ -47,51 +119,4 @@ parse_game_set(std::string_view game_set_line) {
   return game_set;
 }
 
-std::vector<GameSet>
-parse_game_sets(std::string_view game_sets_line) {
-  std::vector<GameSet> game_sets;
-  auto tokens = split(game_sets_line, "; ");
-  std::transform(tokens.begin(),
-                 tokens.end(),
-                 std::back_inserter(game_sets),
-                 parse_game_set);
-  return game_sets;
-}
-
-Game
-parse_game(std::string_view line) {
-  auto tokens = split(line, ": ");
-  return Game{parse_game_id(tokens[0]), parse_game_sets(tokens[1])};
-}
-
-int
-main(int argc, char const **argv) {
-  auto args = std::span(argv, size_t(argc));
-  if (args.size() != 2) {
-    fmt::println(stderr, "usage: {} input.txt", args[0]);
-    return 1;
-  }
-
-  std::ifstream infile(args[1]);
-  if (!infile.is_open()) {
-    fmt::println(stderr, "couldn't open file {}", args[1]);
-    return 2;
-  }
-
-  static constexpr std::size_t MAX_RED = 12;
-  static constexpr std::size_t MAX_GREEN = 13;
-  static constexpr std::size_t MAX_BLUE = 14;
-  std::uint64_t total{};
-  std::string line;
-  while (std::getline(infile, line)) {
-    auto game = parse_game(line);
-    if (std::ranges::all_of(game.m_sets, [](GameSet const &game_set) {
-          return game_set.m_red <= MAX_RED && game_set.m_green <= MAX_GREEN
-                 && game_set.m_blue <= MAX_BLUE;
-        })) {
-      total += game.m_id;
-    }
-  }
-  fmt::println("{}", total);
-  return 0;
-}
+} // namespace
--- src/d02p2.cpp
+++ src/d02p2.cpp
@@ -1,8 +1,9 @@
-#include <algorithm>
-#include <fmt/core.h>
-#include <fstream>
-#include <string>
-#include <vector>
+#include "utility.hpp"
+
+#include <algorithm> // std::ranges::transform
+#include <print> // std::println
+#include <string> // std::string
+#include <vector> // std::vector
 
 struct GameSet
 {
@@ -11,9 +12,7 @@ struct GameSet
   std::size_t m_blue;
 
   [[nodiscard]] std::size_t
-  get_power() const {
-    return m_red * m_green * m_blue;
-  }
+  get_power() const;
 };
 
 struct Game
@@ -22,33 +21,68 @@ struct Game
   std::vector<GameSet> m_sets;
 
   [[nodiscard]] GameSet
-  get_min_game_set() const {
-    GameSet min_game_set{0, 0, 0};
-    for (auto const &game_set : m_sets) {
-      min_game_set.m_red = std::max(min_game_set.m_red, game_set.m_red);
-      min_game_set.m_green = std::max(min_game_set.m_green, game_set.m_green);
-      min_game_set.m_blue = std::max(min_game_set.m_blue, game_set.m_blue);
-    }
-    return min_game_set;
-  }
+  get_min_game_set() const;
 };
 
-std::vector<std::string_view>
-split(std::string_view sv, std::string_view delim = " ") {
-  std::vector<std::string_view> tokens;
-  for (std::size_t right = sv.find(delim); right != std::string_view::npos;
-       right = sv.find(delim)) {
-    if (right == 0) {
-      sv = sv.substr(right + delim.size());
-      continue;
-    }
-    tokens.emplace_back(sv.substr(0, right));
-    sv = sv.substr(right + delim.size());
+namespace
+{
+void
+tests();
+std::uint64_t
+get_sum_of_powers_of_min_game_sets(std::ranges::range auto &&lines);
+Game
+parse_game(std::string_view line);
+std::size_t
+parse_game_id(std::string_view game_id_line);
+std::vector<GameSet>
+parse_game_sets(std::string_view game_sets_line);
+GameSet
+parse_game_set(std::string_view game_set_line);
+} // namespace
+
+int
+main(int argc, char const *const *argv) {
+  tests();
+
+  std::vector<std::string> lines = read_program_input(argc, argv);
+  if (lines.empty()) {
+    return 1;
   }
-  if (!sv.empty()) {
-    tokens.emplace_back(sv);
+
+  std::println("{}", get_sum_of_powers_of_min_game_sets(lines));
+  return 0;
+}
+
+namespace
+{
+void
+tests() {
+  using namespace std::literals::string_view_literals;
+  auto const lines = std::array{
+      "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"sv,
+      "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue"sv,
+      "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red"sv,
+      "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red"sv,
+      "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"sv,
+  };
+  ASSERT(get_sum_of_powers_of_min_game_sets(lines) == 2286);
+}
+
+std::uint64_t
+get_sum_of_powers_of_min_game_sets(std::ranges::range auto &&lines) {
+  std::uint64_t total{};
+  for (auto const &line : lines) {
+    auto game = parse_game(line);
+    total += game.get_min_game_set().get_power();
   }
-  return tokens;
+  return total;
+}
+
+Game
+parse_game(std::string_view line) {
+  auto tokens = split(line, ": ");
+  return Game{.m_id = parse_game_id(tokens[0]),
+              .m_sets = parse_game_sets(tokens[1])};
 }
 
 std::size_t
@@ -61,9 +95,17 @@ parse_game_id(std::string_view game_id_line) {
   return game_id;
 }
 
+std::vector<GameSet>
+parse_game_sets(std::string_view game_sets_line) {
+  std::vector<GameSet> game_sets;
+  auto tokens = split(game_sets_line, "; ");
+  std::ranges::transform(tokens, std::back_inserter(game_sets), parse_game_set);
+  return game_sets;
+}
+
 GameSet
 parse_game_set(std::string_view game_set_line) {
-  GameSet game_set{0, 0, 0};
+  GameSet game_set{.m_red = 0, .m_green = 0, .m_blue = 0};
   for (std::string_view pair : split(game_set_line, ", ")) {
     auto tokens = split(pair);
     std::size_t num{};
@@ -78,44 +120,20 @@ parse_game_set(std::string_view game_set_line) {
   }
   return game_set;
 }
+} // namespace
 
-std::vector<GameSet>
-parse_game_sets(std::string_view game_sets_line) {
-  std::vector<GameSet> game_sets;
-  auto tokens = split(game_sets_line, "; ");
-  std::transform(tokens.begin(),
-                 tokens.end(),
-                 std::back_inserter(game_sets),
-                 parse_game_set);
-  return game_sets;
+std::size_t
+GameSet::get_power() const {
+  return m_red * m_green * m_blue;
 }
 
-Game
-parse_game(std::string_view line) {
-  auto tokens = split(line, ": ");
-  return Game{parse_game_id(tokens[0]), parse_game_sets(tokens[1])};
-}
-
-int
-main(int argc, char const **argv) {
-  auto args = std::span(argv, size_t(argc));
-  if (args.size() != 2) {
-    fmt::println(stderr, "usage: {} input.txt", args[0]);
-    return 1;
+GameSet
+Game::get_min_game_set() const {
+  GameSet min_game_set{.m_red = 0, .m_green = 0, .m_blue = 0};
+  for (GameSet const &game_set : m_sets) {
+    min_game_set.m_red = std::max(min_game_set.m_red, game_set.m_red);
+    min_game_set.m_green = std::max(min_game_set.m_green, game_set.m_green);
+    min_game_set.m_blue = std::max(min_game_set.m_blue, game_set.m_blue);
   }
-
-  std::ifstream infile(args[1]);
-  if (!infile.is_open()) {
-    fmt::println(stderr, "couldn't open file {}", args[1]);
-    return 2;
-  }
-
-  std::uint64_t total{};
-  std::string line;
-  while (std::getline(infile, line)) {
-    auto game = parse_game(line);
-    total += game.get_min_game_set().get_power();
-  }
-  fmt::println("{}", total);
-  return 0;
+  return min_game_set;
 }
