d3ce812b57d0fc7fecff4936f83a5be26017c54a
Christos Gkantidis <cgkantid@proton.me>
2025-09-06 12:53:06 +0200

    Refresh d01 dolutions and add tests

--- src/d01p1.cpp
+++ src/d01p1.cpp
@@ -1,30 +1,55 @@
-#include <fmt/core.h>
-#include <fstream>
-#include <string>
+#include <algorithm> // std::ranges::find_if
+#include <print> // std::println
+#include <ranges> // std::ranges::reverse_view
+#include <string> // std::string
 
 #include "utility.hpp"
 
+namespace
+{
+void
+tests();
+std::uint64_t
+get_sum_of_calibration_values(std::ranges::range auto &&lines);
+} // namespace
+
 int
 main(int argc, char const **argv) {
-  auto args = std::span(argv, size_t(argc));
-  if (args.size() != 2) {
-    fmt::println(stderr, "usage: {} input.txt", args[0]);
+  tests();
+
+  std::vector<std::string> const lines = read_program_input(argc, argv);
+  if (lines.empty()) {
     return 1;
   }
 
-  std::ifstream infile(args[1]);
-  if (!infile.is_open()) {
-    fmt::println(stderr, "couldn't open file {}", args[1]);
-    return 2;
-  }
-
-  std::uint64_t total{};
-  std::string line;
-  while (std::getline(infile, line)) {
-    auto first = std::find_if(line.begin(), line.end(), is_digit);
-    auto last = std::find_if(line.rbegin(), line.rend(), is_digit);
-    total += char_to_int(*first) * 10 + char_to_int(*last);
-  }
-  fmt::println("{}", total);
+  std::println("{}", get_sum_of_calibration_values(lines));
   return 0;
 }
+
+namespace
+{
+void
+tests() {
+  using namespace std::literals::string_view_literals;
+  auto const lines = std::array{
+      "1abc2"sv,
+      "pqr3stu8vwx"sv,
+      "a1b2c3d4e5f"sv,
+      "treb7uchet"sv,
+  };
+  ASSERT(get_sum_of_calibration_values(lines) == 142);
+}
+
+std::uint64_t
+get_sum_of_calibration_values(std::ranges::range auto &&lines) {
+  std::uint64_t total{};
+  for (auto const &line : lines) {
+    // find first digit (from left to right)
+    auto first = std::ranges::find_if(line, is_digit);
+    // find first digit (from right to left)
+    auto last = std::ranges::find_if(std::ranges::reverse_view(line), is_digit);
+    total += char_to_int(*first) * 10UL + char_to_int(*last);
+  }
+  return total;
+}
+} // namespace
--- src/d01p2.cpp
+++ src/d01p2.cpp
@@ -1,56 +1,61 @@
-#include <fmt/core.h>
-#include <fstream>
-#include <ranges>
-#include <string>
+#include "utility.hpp"
+#include <print> // std::println
+#include <ranges> // std::views::enumerate
+#include <string> // std::string
 
-static constexpr auto NUMBERS = std::array<std::pair<char, char const *>, 10>{{
-    {'0', "zero"},
-    {'1', "one"},
-    {'2', "two"},
-    {'3', "three"},
-    {'4', "four"},
-    {'5', "five"},
-    {'6', "six"},
-    {'7', "seven"},
-    {'8', "eight"},
-    {'9', "nine"},
-}};
-
+namespace
+{
+void
+tests();
+std::uint64_t
+get_sum_of_calibration_values(std::ranges::range auto &&lines);
 std::pair<bool, uint8_t>
-str_to_digit(std::string_view sv) {
-  for (auto const &[idx, digit_number] : std::views::enumerate(NUMBERS)) {
-    if (sv.starts_with(digit_number.first)
-        || sv.starts_with(digit_number.second)) {
-      return {true, idx};
-    }
-  }
-  return {false, 0};
-}
+str_to_digit(std::string_view sv);
+} // namespace
 
 int
 main(int argc, char const **argv) {
-  auto args = std::span(argv, size_t(argc));
-  if (args.size() != 2) {
-    fmt::println(stderr, "usage: {} input.txt", args[0]);
+  tests();
+
+  std::vector<std::string> lines = read_program_input(argc, argv);
+  if (lines.empty()) {
     return 1;
   }
 
-  std::ifstream infile(args[1]);
-  if (!infile.is_open()) {
-    fmt::println(stderr, "couldn't open file {}", args[1]);
-    return 2;
-  }
+  std::println("{}", get_sum_of_calibration_values(lines));
+  return 0;
+}
 
+namespace
+{
+void
+tests() {
+  using namespace std::literals::string_view_literals;
+  auto const lines = std::array{
+      "two1nine"sv,
+      "eightwothree"sv,
+      "abcone2threexyz"sv,
+      "xtwone3four"sv,
+      "4nineeightseven2"sv,
+      "zoneight234"sv,
+      "7pqrstsixteen"sv,
+  };
+  ASSERT(get_sum_of_calibration_values(lines) == 281);
+}
+
+std::uint64_t
+get_sum_of_calibration_values(std::ranges::range auto &&lines) {
   std::uint64_t total{};
-  std::string line;
-  while (std::getline(infile, line)) {
+  for (auto const &line : lines) {
     auto const line_len = line.size();
+    // get the first number (from left to right)
     for (std::size_t idx = 0; idx < line_len; ++idx) {
       if (auto const [success, digit] = str_to_digit(line.substr(idx)); success) {
-        total += 10 * digit;
+        total += 10UL * digit;
         break;
       }
     }
+    // get the first digit (from right to left)
     for (std::size_t idx = 0; idx < line_len; ++idx) {
       if (auto const [success, digit] =
               str_to_digit(line.substr(line_len - idx - 1));
@@ -60,6 +65,30 @@ main(int argc, char const **argv) {
       }
     }
   }
-  fmt::println("{}", total);
-  return 0;
+  return total;
 }
+
+std::pair<bool, uint8_t>
+str_to_digit(std::string_view sv) {
+  static constexpr auto NUMBERS = std::array<std::pair<char, char const *>, 10>{{
+      {'0', "zero"},
+      {'1', "one"},
+      {'2', "two"},
+      {'3', "three"},
+      {'4', "four"},
+      {'5', "five"},
+      {'6', "six"},
+      {'7', "seven"},
+      {'8', "eight"},
+      {'9', "nine"},
+  }};
+
+  for (auto const &[idx, digit_number] : std::views::enumerate(NUMBERS)) {
+    if (sv.starts_with(digit_number.first)
+        || sv.starts_with(digit_number.second)) {
+      return {true, idx};
+    }
+  }
+  return {false, 0};
+}
+} // namespace
