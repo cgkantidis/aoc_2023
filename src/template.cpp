11656098c7b7f5dadbe9d5287aea8b2c837f49be
Christos Gkantidis <cgkantid@proton.me>
2025-09-07 23:07:14 +0200

    Add day templace source file

--- src/template.cpp
+++ src/template.cpp
@@ -1,46 +1,20 @@
-#include <array> // std::array
-#include <cstdint> // std::uint64_t
-#include <fmt/core.h> // fmt::print
-#include <fstream> // std::ifstream
-#include <libassert/assert.hpp> // ASSERT
-#include <ranges> // std::views::enumerate
-#include <string> // std::string
-#include <vector> // std::vector
+#include "utility.hpp"
 
-using u64 = std::uint64_t;
-
-using namespace std::literals::string_view_literals;
+#include <print> // std::println
 
 namespace
 {
 void
 tests();
 u64
-get_answer(std::ranges::range auto &&lines);
+get_num_lines(std::ranges::range auto &&lines);
 } // namespace
 
 int
-main(int argc, char const *const *argv) {
+main(int argc, char const **argv) {
   tests();
-
-  auto args = std::span(argv, size_t(argc));
-  if (args.size() != 2) {
-    fmt::println(stderr, "usage: {} input.txt", args[0]);
-    return 1;
-  }
-
-  std::ifstream infile(args[1]);
-  if (!infile.is_open()) {
-    fmt::println(stderr, "couldn't open file {}", args[1]);
-    return 2;
-  }
-
-  std::vector<std::string> lines;
-  for (std::string line; std::getline(infile, line);) {
-    lines.emplace_back(std::move(line));
-  }
-
-  fmt::println("{}", get_answer(lines));
+  std::vector<std::string> lines = read_program_input(argc, argv);
+  std::println("{}", get_num_lines(lines));
   return 0;
 }
 
@@ -49,17 +23,15 @@ namespace
 void
 tests() {
   using namespace std::literals::string_view_literals;
-  {
-    auto const lines = std::array{
-        ""sv,
-    };
-    ASSERT(get_answer(lines) == 42);
-  }
+  auto const lines = std::array{
+      "line1"sv,
+      "line2"sv,
+  };
+  ASSERT(get_num_lines(lines) == 2);
 }
 
 u64
-get_answer(std::ranges::range auto &&lines) {
+get_num_lines(std::ranges::range auto &&lines) {
   return lines.size();
 }
 } // namespace
-
