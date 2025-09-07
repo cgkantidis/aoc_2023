16a61e5926b2b53729fec433e71fb79e4c7828ea
Christos Gkantidis <cgkantid@proton.me>
2025-09-07 23:08:48 +0200

    Refresh d05 dolutions and add tests

--- src/d05p1.cpp
+++ src/d05p1.cpp
@@ -1,103 +1,152 @@
-#include <algorithm>
-#include <fmt/core.h>
-#include <fstream>
-#include <ranges>
-#include <set>
-#include <string>
-#include <vector>
+#include <algorithm> // std::ranges::sort
+#include <print> // std::println
+#include <ranges> // std::views::enumerate
 
 #include "utility.hpp"
 
 struct mapping
 {
-  std::size_t dst;
-  std::size_t src;
-  std::size_t sz;
+  u64 dst;
+  u64 src;
+  u64 sz;
 };
 
-std::vector<std::size_t>
-parse_seeds(std::ifstream &infile) {
-  std::string line;
-  std::getline(infile, line);
-  auto str_seeds = split(split(line, "seeds: ")[0]);
-  std::vector<std::size_t> seeds;
-  std::ranges::transform(str_seeds, std::back_inserter(seeds), str_to_int<std::size_t>);
-  std::getline(infile, line); // empty line
-  return seeds;
+namespace
+{
+void
+tests();
+u64
+get_min_location_for_seeds(std::vector<std::string> const &lines);
+std::vector<std::span<std::string const>>
+get_blocks(std::vector<std::string> const &lines);
+std::vector<u64>
+parse_seeds(std::span<std::string const> lines);
+std::vector<u64>
+parse_and_map(std::span<std::string const> lines,
+              std::vector<u64> const &values);
+u64
+convert(u64 value, std::vector<mapping> const &map);
+} // namespace
+
+int
+main(int argc, char const **argv) {
+  tests();
+  std::vector<std::string> lines = read_program_input(argc, argv);
+  std::println("{}", get_min_location_for_seeds(lines));
+  return 0;
 }
 
-std::size_t
-convert(std::size_t value, std::vector<mapping> const &map) {
+namespace
+{
+void
+tests() {
+  std::vector<std::string> const lines{
+      "seeds: 79 14 55 13",
+      "",
+      "seed-to-soil map:",
+      "50 98 2",
+      "52 50 48",
+      "",
+      "soil-to-fertilizer map:",
+      "0 15 37",
+      "37 52 2",
+      "39 0 15",
+      "",
+      "fertilizer-to-water map:",
+      "49 53 8",
+      "0 11 42",
+      "42 0 7",
+      "57 7 4",
+      "",
+      "water-to-light map:",
+      "88 18 7",
+      "18 25 70",
+      "",
+      "light-to-temperature map:",
+      "45 77 23",
+      "81 45 19",
+      "68 64 13",
+      "",
+      "temperature-to-humidity map:",
+      "0 69 1",
+      "1 0 69",
+      "",
+      "humidity-to-location map:",
+      "60 56 37",
+      "56 93 4",
+  };
+  ASSERT(get_min_location_for_seeds(lines) == 35);
+}
+
+u64
+get_min_location_for_seeds(std::vector<std::string> const &lines) {
+  auto const blocks = get_blocks(lines);
+  std::vector<u64> const seeds = parse_seeds(blocks[0]);
+  auto soil = parse_and_map(blocks[1], seeds);
+  auto fertilizer = parse_and_map(blocks[2], soil);
+  auto water = parse_and_map(blocks[3], fertilizer);
+  auto light = parse_and_map(blocks[4], water);
+  auto temperature = parse_and_map(blocks[5], light);
+  auto humidity = parse_and_map(blocks[6], temperature);
+  auto location = parse_and_map(blocks[7], humidity);
+  return std::ranges::min(location);
+}
+
+std::vector<std::span<std::string const>>
+get_blocks(std::vector<std::string> const &lines) {
+  std::vector<std::span<std::string const>> spans;
+  auto beg_it = lines.begin();
+  while (true) {
+    auto find_it =
+        std::find_if(beg_it, lines.end(), [](std::string const &line) {
+          return line.empty();
+        });
+    spans.emplace_back(beg_it, find_it);
+    if (find_it == lines.end()) {
+      break;
+    }
+    beg_it = std::next(find_it);
+  }
+  return spans;
+}
+
+std::vector<u64>
+parse_seeds(std::span<std::string const> lines) {
+  auto str_seeds = split(split(lines[0], "seeds: ")[0]);
+  return std::views::transform(str_seeds, str_to_int<u64>)
+         | std::ranges::to<std::vector<u64>>();
+}
+
+std::vector<u64>
+parse_and_map(std::span<std::string const> lines,
+              std::vector<u64> const &values) {
+  std::vector<mapping> map;
+  for (std::string const &line : lines) {
+    auto tokens = split(line) | std::views::transform(str_to_int<u64>);
+    map.emplace_back(tokens[0], tokens[1], tokens[2]);
+  }
+
+  std::ranges::sort(map, [](mapping const &left, mapping const &right) {
+    return left.src < right.src;
+  });
+
+  return std::views::transform(
+             values,
+             [&map](u64 const &value) { return convert(value, map); })
+         | std::ranges::to<std::vector<u64>>();
+}
+
+u64
+convert(u64 value, std::vector<mapping> const &map) {
   for (mapping const &m : map) {
     if (value < m.src) {
       // value is not mapped
       return value;
     }
-    if (value >= m.src && value < m.src + m.sz) {
+    if (value < m.src + m.sz) {
       return m.dst + value - m.src;
     }
   }
   return value;
 }
-
-std::vector<std::size_t>
-parse_and_map(std::ifstream &infile, std::vector<std::size_t> const &values) {
-  std::vector<mapping> map;
-  bool is_first = true;
-  for (std::string line; std::getline(infile, line) && !line.empty();) {
-    if (is_first) {
-      is_first = false;
-      continue;
-    }
-    auto tokens = split(line) | std::views::transform(str_to_int<std::size_t>);
-    map.emplace_back(tokens[0], tokens[1], tokens[2]);
-  }
-  std::ranges::sort(map, [](mapping const &left, mapping const &right) {
-    return left.src < right.src;
-  });
-
-  std::vector<std::size_t> new_values;
-  std::ranges::transform(values, std::back_inserter(new_values), [&map](std::size_t const &value) {
-    return convert(value, map);
-  });
-  return new_values;
-}
-
-std::size_t
-min(std::vector<std::size_t> const &values) {
-  std::size_t min_value{std::numeric_limits<std::size_t>::max()};
-  for (std::size_t const &value : values) {
-    if (value < min_value) {
-      min_value = value;
-    }
-  }
-  return min_value;
-}
-
-int
-main(int argc, char const **argv) {
-  auto args = std::span(argv, size_t(argc));
-  if (args.size() != 2) {
-    fmt::println(stderr, "usage: {} input.txt", args[0]);
-    return 1;
-  }
-
-  std::ifstream infile(args[1]);
-  if (!infile.is_open()) {
-    fmt::println(stderr, "couldn't open file {}", args[1]);
-    return 2;
-  }
-
-  auto seeds = parse_seeds(infile);
-  auto soil = parse_and_map(infile, seeds);
-  auto fertilizer = parse_and_map(infile, soil);
-  auto water = parse_and_map(infile, fertilizer);
-  auto light = parse_and_map(infile, water);
-  auto temperature = parse_and_map(infile, light);
-  auto humidity = parse_and_map(infile, temperature);
-  auto location = parse_and_map(infile, humidity);
-
-  fmt::println("{}", min(location));
-
-  return 0;
-}
+} // namespace
--- src/d05p2.cpp
+++ src/d05p2.cpp
@@ -1,113 +1,206 @@
-#include <algorithm>
-#include <fmt/core.h>
-#include <fstream>
-#include <numeric>
-#include <ranges>
-#include <string>
-#include <vector>
+#include <algorithm> // std::ranges::sort
+#include <print> // std::println
+#include <ranges> // std::views::enumerate
 
 #include "utility.hpp"
 
 struct mapping
 {
-  std::size_t dst;
-  std::size_t src;
-  std::size_t sz;
+  u64 dst;
+  u64 src;
+  u64 sz;
 };
 
-std::vector<std::size_t>
-parse_seeds(std::ifstream &infile) {
-  std::string line;
-  std::getline(infile, line);
-  auto str_seeds = split(split(line, "seeds: ")[0]);
-  std::vector<std::size_t> seeds;
-  std::ranges::transform(str_seeds, std::back_inserter(seeds), str_to_int<std::size_t>);
-  std::getline(infile, line); // empty line
-  return seeds;
-}
+struct range
+{
+  // range from [src, dst)
+  u64 src;
+  u64 dst;
+  u64 sz;
+};
 
-std::size_t
-convert(std::size_t value, std::vector<mapping> const &map) {
-  for (mapping const &m : map) {
-    if (value < m.src) {
-      // value is not mapped
-      return value;
-    }
-    if (value < m.src + m.sz) {
-      return m.dst + value - m.src;
-    }
-  }
-  return value;
-}
-
-std::size_t
-convert(std::size_t value, std::vector<std::vector<mapping>> const &mappings) {
-  return std::accumulate(
-      mappings.begin(),
-      mappings.end(),
-      value,
-      [](std::size_t prev_value, std::vector<mapping> const &map) {
-        return convert(prev_value, map);
-      });
-}
-
+namespace
+{
+void
+tests();
+u64
+get_min_location_for_seeds(std::vector<std::string> const &lines);
+std::vector<range>
+tranform_range_by_mapping(range const &r, std::vector<mapping> const &mappings);
+std::vector<std::span<std::string const>>
+get_blocks(std::vector<std::string> const &lines);
+std::vector<range>
+get_seed_ranges(std::span<std::string const> lines);
 std::vector<std::vector<mapping>>
-parse_mappings(std::ifstream &infile) {
-  std::vector<std::vector<mapping>> mappings(7);
-  for (std::vector<mapping> &map : mappings) {
-    bool is_first = true;
-    for (std::string line; std::getline(infile, line) && !line.empty();) {
-      if (is_first) {
-        is_first = false;
-        continue;
-      }
-      auto tokens = split(line) | std::views::transform(str_to_int<std::size_t>);
-      map.emplace_back(tokens[0], tokens[1], tokens[2]);
-    }
-    std::ranges::sort(map, [](mapping const &left, mapping const &right) {
-      return left.src < right.src;
-    });
-  }
-  return mappings;
-}
-
-std::size_t
-min(std::vector<std::size_t> const &values) {
-  std::size_t min_value{std::numeric_limits<std::size_t>::max()};
-  for (std::size_t const &value : values) {
-    if (value < min_value) {
-      min_value = value;
-    }
-  }
-  return min_value;
-}
+get_mappings(std::vector<std::span<std::string const>> const &blocks);
+std::vector<mapping>
+parse_map(std::span<std::string const> lines);
+} // namespace
 
 int
 main(int argc, char const **argv) {
-  auto args = std::span(argv, size_t(argc));
-  if (args.size() != 2) {
-    fmt::println(stderr, "usage: {} input.txt", args[0]);
-    return 1;
-  }
-
-  std::ifstream infile(args[1]);
-  if (!infile.is_open()) {
-    fmt::println(stderr, "couldn't open file {}", args[1]);
-    return 2;
-  }
-
-  auto seeds = parse_seeds(infile);
-  auto mappings = parse_mappings(infile);
-  std::size_t min_location{std::numeric_limits<std::size_t>::max()};
-  for (auto chunk : std::views::chunk(seeds, 2)) {
-    for (std::size_t seed = chunk[0]; seed < chunk[0] + chunk[1]; ++seed) {
-      auto location = convert(seed, mappings);
-      if (location < min_location) {
-        min_location = location;
-      }
-    }
-  }
-  fmt::println("{}", min_location);
-
+  tests();
+  std::vector<std::string> lines = read_program_input(argc, argv);
+  std::println("{}", get_min_location_for_seeds(lines));
   return 0;
 }
+
+namespace
+{
+void
+tests() {
+  std::vector<std::string> const lines{
+      "seeds: 79 14 55 13",
+      "",
+      "seed-to-soil map:",
+      "50 98 2",
+      "52 50 48",
+      "",
+      "soil-to-fertilizer map:",
+      "0 15 37",
+      "37 52 2",
+      "39 0 15",
+      "",
+      "fertilizer-to-water map:",
+      "49 53 8",
+      "0 11 42",
+      "42 0 7",
+      "57 7 4",
+      "",
+      "water-to-light map:",
+      "88 18 7",
+      "18 25 70",
+      "",
+      "light-to-temperature map:",
+      "45 77 23",
+      "81 45 19",
+      "68 64 13",
+      "",
+      "temperature-to-humidity map:",
+      "0 69 1",
+      "1 0 69",
+      "",
+      "humidity-to-location map:",
+      "60 56 37",
+      "56 93 4",
+  };
+  ASSERT(get_min_location_for_seeds(lines) == 46);
+}
+
+u64
+get_min_location_for_seeds(std::vector<std::string> const &lines) {
+  auto const blocks = get_blocks(lines);
+  std::vector<range> seed_ranges = get_seed_ranges(blocks[0]);
+  std::vector<std::vector<mapping>> mappings = get_mappings(blocks);
+
+  for (std::vector<mapping> const &mapping_group : mappings) {
+    std::vector<range> new_ranges;
+    for (range const &r : seed_ranges) {
+      new_ranges.append_range(tranform_range_by_mapping(r, mapping_group));
+    }
+    seed_ranges = std::move(new_ranges);
+  }
+
+  return std::ranges::min(seed_ranges, {}, [](range const &r) { return r.src; })
+      .src;
+}
+
+range
+convert(range const &r, mapping const &m) {
+  u64 src = std::max(r.src, m.src);
+  u64 sz = std::min(r.dst, m.src + m.sz) - src;
+  return {.src = src + m.dst - m.src, .dst = src + sz + m.dst - m.src, .sz = sz};
+}
+
+bool
+is_overlapping(range const &r, mapping const &m) {
+  return r.dst > m.src && r.src < m.src + m.sz;
+};
+
+std::vector<range>
+tranform_range_by_mapping(range const &r, std::vector<mapping> const &mappings) {
+  auto const overlapping_mappings =
+      std::views::filter(mappings,
+                         [&r](mapping const &m) { return is_overlapping(r, m); })
+      | std::ranges::to<std::vector<mapping>>();
+
+  std::vector<range> new_ranges;
+  for (auto const &[idx, m] : std::views::enumerate(overlapping_mappings)) {
+    if (idx == 0) {
+      // see if there is a left trailing part only for the first
+      // any possible right trailing part will be handled later
+      if (r.src < m.src) {
+        u64 left_sz = m.src - r.src;
+        new_ranges.emplace_back(r.src, r.src + left_sz, left_sz);
+      }
+    }
+    new_ranges.emplace_back(convert(r, m));
+
+    if (r.dst <= m.src + m.sz) {
+      continue;
+    }
+    u64 src = m.src + m.sz;
+    u64 sz = static_cast<u64>(idx) == overlapping_mappings.size() - 1
+                 ? r.dst - src
+                 : std::min(r.dst - src,
+                            overlapping_mappings[static_cast<u64>(idx) + 1].src
+                                - m.src - m.sz);
+    new_ranges.emplace_back(src, src + sz, sz);
+  }
+
+  if (new_ranges.empty()) {
+    return {r};
+  }
+
+  return new_ranges;
+}
+
+std::vector<std::span<std::string const>>
+get_blocks(std::vector<std::string> const &lines) {
+  std::vector<std::span<std::string const>> spans;
+  auto beg_it = lines.begin();
+  while (true) {
+    auto find_it =
+        std::find_if(beg_it, lines.end(), [](std::string const &line) {
+          return line.empty();
+        });
+    spans.emplace_back(beg_it, find_it);
+    if (find_it == lines.end()) {
+      break;
+    }
+    beg_it = std::next(find_it);
+  }
+  return spans;
+}
+
+std::vector<range>
+get_seed_ranges(std::span<std::string const> lines) {
+  auto str_seeds = split(split(lines[0], "seeds: ")[0]);
+  return str_seeds | std::views::transform(str_to_int<u64>)
+         | std::views::chunk(2) | std::views::transform([](auto const &chunk) {
+             return range{chunk[0], chunk[0] + chunk[1], chunk[1]};
+           })
+         | std::ranges::to<std::vector<range>>();
+}
+
+std::vector<std::vector<mapping>>
+get_mappings(std::vector<std::span<std::string const>> const &blocks) {
+  return std::views::transform(std::views::drop(blocks, 1), parse_map)
+         | std::ranges::to<std::vector<std::vector<mapping>>>();
+}
+
+std::vector<mapping>
+parse_map(std::span<std::string const> lines) {
+  std::vector<mapping> map;
+  for (std::string const &line : std::views::drop(lines, 1)) {
+    auto tokens = split(line) | std::views::transform(str_to_int<u64>);
+    map.emplace_back(tokens[0], tokens[1], tokens[2]);
+  }
+
+  std::ranges::sort(map, [](mapping const &left, mapping const &right) {
+    return left.src < right.src;
+  });
+
+  return map;
+}
+} // namespace
